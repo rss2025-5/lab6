e7569f466d56bf4bba1f79c13027fe6386329710
Jialin Chen <jialinc7@mit.edu>
2025-04-14 18:01:42 -0400

    px transforms

--- path_planning/trajectory_planner.py
+++ path_planning/trajectory_planner.py
@@ -6,6 +6,9 @@ from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped, PoseArray
 from nav_msgs.msg import OccupancyGrid
 from .utils import LineTrajectory
 
+from tf_transformations import euler_from_quaternion
+import numpy as np
+
 
 class PathPlan(Node):
     """ Listens for goal pose published by RViz and uses it to plan a path from
@@ -50,18 +53,87 @@ class PathPlan(Node):
 
         self.trajectory = LineTrajectory(node=self, viz_namespace="/planned_trajectory")
 
+        # got:
+        self.goal_pose_stamped = PoseStamped()
+        self.map_grid = OccupancyGrid()
+        self.init_pose = PoseWithCovarianceStamped()
+        self.resolution = 1.0
+        self.origin_orientation = 0 # should be Quaternion
+        self.origin_position = 0 # should be Point
+
+
     def map_cb(self, msg):
-        raise NotImplementedError
+        self.map_grid = msg
+        data = msg.data
+
+
+        self.resolution = msg.info.resolution
+        self.origin_orientation = msg.info.origin.orientation
+        self.origin_position = msg.info.origin.position
+
+        self.get_logger().info(f"map_info:{msg.info}")
 
     def pose_cb(self, pose):
-        raise NotImplementedError
+        self.init_pose = pose
 
     def goal_cb(self, msg):
-        raise NotImplementedError
+        self.goal_pose_stamped = msg
+
+
+        self.plan_path(self.init_pose, self.goal_pose_stamped, self.map_grid)
 
     def plan_path(self, start_point, end_point, map):
         self.traj_pub.publish(self.trajectory.toPoseArray())
         self.trajectory.publish_viz()
+
+
+    def create_transform_matrix_2d(self, theta, tx, ty):
+        c = np.cos(theta)
+        s = np.sin(theta)
+        return np.array([
+            [c, -s, tx],
+            [s,  c, ty],
+            [0,  0, 1]
+        ])
+
+    def pixel_to_real(self, u, v, msg):
+        # Step 1: scale
+        resolution = msg.info.resolution
+        scaled_x = u * resolution
+        scaled_y = v * resolution
+        pixel_hom = np.array([scaled_x, scaled_y, 1.0])  # homogeneous
+
+        # Step 2: rotation and translation
+        q = msg.info.origin.orientation
+        _, _, theta = euler_from_quaternion((q.x, q.y, q.z, q.w))
+
+        tx = msg.info.origin.position.x
+        ty = msg.info.origin.position.y
+
+        T = self.create_transform_matrix_2d(theta, tx, ty)
+
+        world = T @ pixel_hom
+        return world[0], world[1]
+
+    def real_to_pixel(self, x, y, msg):
+        # Step 1: get inverse transform matrix
+        q = msg.info.origin.orientation
+        _, _, theta = euler_from_quaternion((q.x, q.y, q.z, q.w))
+        tx = msg.info.origin.position.x
+        ty = msg.info.origin.position.y
+
+        T = self.create_transform_matrix_2d(theta, tx, ty)
+        T_inv = np.linalg.inv(T)
+
+        # Step 2: apply inverse transform
+        world_hom = np.array([x, y, 1.0])
+        pixel_hom = T_inv @ world_hom
+
+        # Step 3: scale down to pixel coords
+        resolution = msg.info.resolution
+        u = pixel_hom[0] / resolution
+        v = pixel_hom[1] / resolution
+        return int(round(u)), int(round(v))
 
 
 def main(args=None):
